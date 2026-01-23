cb942ad9cc1622aae952f1df6eceb23168ef3ab9
Ruta Klimaviciute <rutaklim7@gmail.com>
2026-01-23 00:43:29 +0000

    fixed the structure for intellij

---
+++ src/Notes_and_crosses.java
@@ -0,0 +1,31 @@
+// import Table.java;
+// sess 1:
+
+/*
+PLAN
+- have a method that prints out the notes and crosses grid with a parameter of what square is chosen and what player for
+- another method for checking if there is a valid winning result
+
+1. print grid
+2. randomly allocate which player gets to go first
+3. when a player has entered a VALID command
+4. swap to the second player
+5. end the game once, there is a winner
+6. give option to save result to a text file, and read result from that text file
+7. give option to ask if to restart game/ play another
+
+extra for fun:
+    STATS:
+        - fastest time for a win
+        - most wins from a player
+        - longest game
+        - leaderboard
+
+ */
+
+public class Notes_and_crosses {
+    public static void main(String [] args) {
+        System.out.println("WORK PLS!!");
+    }
+
+}
